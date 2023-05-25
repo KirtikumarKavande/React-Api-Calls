32e80e2926f47747269c7c352193a693aa56db6a
KirtikumarKavande <kirtikumar0005@gmail.com>
2023-05-25 17:46:30 +0530

    async await func updated

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movieList, setMovieList] = useState([]);
+  const dummyMovies = [
+    {
+      id: 1,
+      title: "Some Dummy Movie",
+      openingText: "This is the opening text of the movie",
+      releaseDate: "2021-05-18",
+    },
+    {
+      id: 2,
+      title: "Some Dummy Movie 2",
+      openingText: "This is the second opening text of the movie",
+      releaseDate: "2021-05-19",
+    },
+  ];
+  const HandlefetchMovieData = async () => {
+    const data = await fetch("https://swapi.dev/api/films");
+    const gotData = await data.json();
+
+    const updatedArray = gotData.results.map((item) => {
+      return {
+        id: item.episode_id,
+        title: item.title,
+        openingText: item.opening_crawl,
+        releaseDate: item.release_date,
+      };
+    });
+    setMovieList(updatedArray);
+  };
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={HandlefetchMovieData}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movieList} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
