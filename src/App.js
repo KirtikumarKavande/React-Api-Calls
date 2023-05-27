36bb920989c9e7219173b400c9ece0c161fc5167
KirtikumarKavande <kirtikumar0005@gmail.com>
2023-05-27 11:22:19 +0530

    useEffect added

--- src/App.js
+++ src/App.js
@@ -1,41 +1,61 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
+import { useCallback } from "react";
 
 function App() {
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-
-  const HandlefetchMovieData = async () => {
-    setIsLoading(true);
-    const data = await fetch("https://swapi.dev/api/films");
-    const gotData = await data.json();
-    console.log("---------------------")
-
-    setIsLoading(false);
-
-    const updatedArray = gotData.results.map((item) => {
-      return {
-        id: item.episode_id,
-        title: item.title,
-        openingText: item.opening_crawl,
-        releaseDate: item.release_date,
-      };
-    });
-    setMovieList(updatedArray);
+  const [showError, setShowError] = useState(null);
+  const [clearTimeInterval, setClearTimeInterval] = useState(false);
+  const clearTimeIntervalFunc = () => {
+    setClearTimeInterval(true);
   };
-  console.log("re-evalute")
+
+  
+
+  const dataStored = useCallback(async () => {
+    try {
+      setIsLoading(true);
+
+      const data = await fetch("https://swapi.dev/api/films");
+
+      if (!data.ok) {
+        throw new Error("something went retrying");
+      }
+
+      const gotData = await data.json();
+
+      setIsLoading(false);
+
+      const updatedArray = gotData.results.map((item) => {
+        return {
+          id: item.episode_id,
+          title: item.title,
+          openingText: item.opening_crawl,
+          releaseDate: item.release_date,
+        };
+      });
+      setMovieList(updatedArray);
+    } catch (error) {
+      setShowError(error.message);
+    }
+  },[]);
+  useEffect(() => {
+    dataStored();
+  }, [dataStored]);
 
   return (
     <React.Fragment>
-      {console.log("render")}
+      <section></section>
       <section>
-        <button onClick={HandlefetchMovieData}>Fetch Movies</button>
-      </section>
-      <section>
-        {isLoading && <p>Loading...</p>}
+        {isLoading && !showError && <p>Loading...</p>}
         {!isLoading && <MoviesList movies={movieList} />}
+        {!isLoading && movieList.length === 0 && <p>click on fetch</p>}
+        {showError && <button onClick={clearTimeIntervalFunc}>cancal</button>}
+
+        <p>{showError}</p>
       </section>
     </React.Fragment>
   );
